c7c720753d3bc02b60bef3729994df7f09cade5e
Matheus Costa <matheus.bionicle@gmail.com>
2018-05-25 01:13:11 -0300

    Fix check grid and some other fixes

--- Sudoku/file.c
+++ Sudoku/file.c
@@ -2,118 +2,120 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool check_row(int matriz[9][9]){
-    int i, j, k;
-    bool pass = true;
+typedef struct array {
+        int subgrid[3][3];
+} Array;
 
-    for(i = 0; i < 9 && pass; i++)
-        for(j = 0; j < 9 && pass; j++)
-            for(k = 0; k < 9 && pass; k++)
-                if((j != k) && (matriz[i][j] == matriz[i][k])){
-                    pass = false;
-                    printf("matriz[%d][%d] == matriz[%d][%d] \n",i,j,i,k);
-                }
-    return pass;
-}
+bool check_row(int matrix[9][9]){
+        int i, j, k;
+        bool pass = true;
 
-bool check_column(int matriz[9][9]){
-    int i, j, k;
-    bool pass = true;
-
-    for(j = 0; j < 9 && pass; j++)
         for(i = 0; i < 9 && pass; i++)
-            for(k = 0; k < 9 && pass; k++)
-                if((i != k) && (matriz[i][j] == matriz[k][j])) {
-                    pass = false;
-                    printf("matriz[%d][%d] == matriz[%d][%d] \n",i,j,k,j);
+                for(j = 0; j < 9 && pass; j++)
+                        for(k = 0; k < 9 && pass; k++)
+                                if((j != k) && (matrix[i][j] == matrix[i][k])) {
+                                        pass = false;
+                                        printf("matriz[%d][%d] == matriz[%d][%d] \n",i,j,i,k);
+                                }
+        return pass;
+}
+
+bool check_column(int matrix[9][9]){
+        int i, j, k;
+        bool pass = true;
+
+        for(j = 0; j < 9 && pass; j++)
+                for(i = 0; i < 9 && pass; i++)
+                        for(k = 0; k < 9 && pass; k++)
+                                if((i != k) && (matrix[i][j] == matrix[k][j])) {
+                                        pass = false;
+                                        printf("matriz[%d][%d] == matriz[%d][%d] \n",i,j,k,j);
+                                }
+        return pass;
+}
+
+Array divide_matriz(int matrix[9][9], int init_row, int init_column) {
+        Array sub_matrix;
+
+        for(int i = 0; i < 3; ++i) {
+                for(int j = 0; j < 3; ++j) {
+                        sub_matrix.subgrid[i][j] = matrix[i + init_row][j + init_column];
                 }
-    return pass;
+        }
+        return sub_matrix;
 }
 
-//funcao generica apenas para poder testar a check_grid
-int divide_matriz(int matriz[9][9], int submatriz[3][3], int i_init, int j_init) {
-      submatriz[0][0] = matriz[i_init][j_init];
-      submatriz[0][1] = matriz[i_init][j_init+1];
-      submatriz[0][2] = matriz[i_init][j_init+2];
-      submatriz[1][0] = matriz[i_init+1][j_init];
-      submatriz[1][1] = matriz[i_init+1][j_init+1];
-      submatriz[1][2] = matriz[i_init+1][j_init+2];
-      submatriz[2][0] = matriz[i_init+2][j_init];
-      submatriz[2][1] = matriz[i_init+2][j_init+1];
-      submatriz[2][2] = matriz[i_init+2][j_init+2];
-}
+void check_grid(Array sub_matrix, int id_grid) {
+        int sum = 0;
 
-bool check_grid(int subgrid[3][3], int id_grid) {
-  int i, j, k, exist = 0;
-  bool pass = true;
-
-  for(k = 0; k < 9 && pass; k++) {
-    for(i = 0; i < 3 && pass; i++) 
-      for(j = 0; j < 3 && pass; j++)
-        if(subgrid[i][j] == k) exist++;
-              if(exist > 1) pass = false;
-                  exist = 0;
-  }
-  if(pass == true)
-  printf("Grid %d valido, nenhum numero se repete.\n", id_grid);
-    return pass;
+        for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                        sum += sub_matrix.subgrid[i][j];
+                }
+        }
+        if (sum == 45)
+                printf("Grid %d valido, nenhum numero se repete.\n", id_grid);
+        else {
+                printf("Grid %d invalido, algum numero se repete.\n", id_grid);
+        }
 }
 
 int main() {
-  FILE *sudoku;
-  int subgrid_1[3][3], subgrid_2[3][3], subgrid_3[3][3], subgrid_4[3][3],
-      subgrid_5[3][3], subgrid_6[3][3], subgrid_7[3][3], subgrid_8[3][3], subgrid_9[3][3];
-  int sudoku_number, sudoku_game[9][9];
-  char sudoku_file[15];
-  printf("Digite o numero do sudoku: ");
-  scanf("%d", &sudoku_number);
-  sprintf(sudoku_file, "sudokus/%d.txt", sudoku_number);
-  printf("%s\n", sudoku_file);
-  sudoku = fopen(sudoku_file, "r");
-  if(sudoku == NULL) {
-    printf("Este arquivo nao existe!\n");
-    return 0;
-  }
-  for(int i = 0; i < 9; ++i) {
-    for(int j = 0; j < 9; ++j) {
-      fscanf(sudoku, "%d", &sudoku_game[i][j]);
-    }
-  }
-  for(int i = 0; i < 9; ++i) {
-    for(int j = 0; j < 9; ++j) {
-      printf("%d ", sudoku_game[i][j]);
-    }
-    printf("\n");
-  }
-  if(check_row(sudoku_game)){
-    printf("Nao ha nenhum numero repetido entre as linhas.\n");
-  } else {
-    printf("^ Esses numeros estao se repetindo na linha! ^\n");
-  }
-  if(check_column(sudoku_game)){
-    printf("Nao ha nenhum numero repetido entre as colunas.\n");
-  } else {
-    printf("^ Esses numeros estao se repetindo na coluna!\n");
-  }
-  divide_matriz(sudoku_game, subgrid_1, 0, 0);
-  divide_matriz(sudoku_game, subgrid_2, 0, 3);
-  divide_matriz(sudoku_game, subgrid_3, 0, 6);
-  divide_matriz(sudoku_game, subgrid_4, 3, 0);
-  divide_matriz(sudoku_game, subgrid_5, 3, 3);
-  divide_matriz(sudoku_game, subgrid_6, 3, 6);
-  divide_matriz(sudoku_game, subgrid_7, 6, 0);
-  divide_matriz(sudoku_game, subgrid_8, 6, 3);
-  divide_matriz(sudoku_game, subgrid_9, 6, 6);
-  check_grid(subgrid_1, 1);
-  check_grid(subgrid_2, 2);
-  check_grid(subgrid_3, 3);
-  check_grid(subgrid_4, 4);
-  check_grid(subgrid_5, 5);
-  check_grid(subgrid_6, 6);
-  check_grid(subgrid_7, 7);
-  check_grid(subgrid_8, 8);
-  check_grid(subgrid_9, 9);
-  
-  fclose(sudoku);
-  return 0;
+        FILE *sudoku;
+        Array subgrid_1, subgrid_2, subgrid_3, subgrid_4, subgrid_5,
+              subgrid_6, subgrid_7, subgrid_8, subgrid_9;
+
+        int sudoku_number, sudoku_game[9][9];
+        char sudoku_file[15];
+        printf("Digite o numero do sudoku: ");
+        scanf("%d", &sudoku_number);
+        sprintf(sudoku_file, "sudokus/%d.txt", sudoku_number);
+        printf("%s\n", sudoku_file);
+        sudoku = fopen(sudoku_file, "r");
+        if(sudoku == NULL) {
+                printf("Este arquivo nao existe!\n");
+                return 0;
+        }
+        for(int i = 0; i < 9; ++i) {
+                for(int j = 0; j < 9; ++j) {
+                        fscanf(sudoku, "%d", &sudoku_game[i][j]);
+                }
+        }
+        for(int i = 0; i < 9; ++i) {
+                for(int j = 0; j < 9; ++j) {
+                        printf("%d ", sudoku_game[i][j]);
+                }
+                printf("\n");
+        }
+        if(check_row(sudoku_game)) {
+                printf("Nao ha nenhum numero repetido entre as linhas.\n");
+        } else {
+                printf("^ Esses numeros estao se repetindo na linha! ^\n");
+        }
+        if(check_column(sudoku_game)) {
+                printf("Nao ha nenhum numero repetido entre as colunas.\n");
+        } else {
+                printf("^ Esses numeros estao se repetindo na coluna! ^\n");
+        }
+        subgrid_1 = divide_matriz(sudoku_game, 0, 0);
+        subgrid_2 = divide_matriz(sudoku_game, 0, 3);
+        subgrid_3 = divide_matriz(sudoku_game, 0, 6);
+        subgrid_4 = divide_matriz(sudoku_game, 3, 0);
+        subgrid_5 = divide_matriz(sudoku_game, 3, 3);
+        subgrid_6 = divide_matriz(sudoku_game, 3, 6);
+        subgrid_7 = divide_matriz(sudoku_game, 6, 0);
+        subgrid_8 = divide_matriz(sudoku_game, 6, 3);
+        subgrid_9 = divide_matriz(sudoku_game, 6, 6);
+        check_grid(subgrid_1, 1);
+        check_grid(subgrid_2, 2);
+        check_grid(subgrid_3, 3);
+        check_grid(subgrid_4, 4);
+        check_grid(subgrid_5, 5);
+        check_grid(subgrid_6, 6);
+        check_grid(subgrid_7, 7);
+        check_grid(subgrid_8, 8);
+        check_grid(subgrid_9, 9);
+
+        fclose(sudoku);
+        return 0;
 }
